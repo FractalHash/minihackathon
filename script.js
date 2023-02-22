25dc524118fc446444dfd77e86d1d7c4c2bcc28c
Trevor5008 <trevorwerner@hotmail.com>
2023-02-22 15:26:32 -0500

    Fixed event listener

--- script.js
+++ script.js
@@ -5,43 +5,44 @@ const imgEl = document.querySelector(".cocktail-img");
 const nameEl = document.querySelector(".cocktail-name");
 const ingredientsList = document.querySelector(".cocktail-ingredients");
 
-const promise = axios.get(pathURL).then((res) => {
-  cocktailData = res.data.drinks[0];
+const promiseFunc = () => {
+   return axios.get(pathURL).then((res) => {
+      cocktailData = res.data.drinks[0];
 
-  //name
-  const cocktailName = cocktailData.strDrink;
-  nameEl.innerHTML = cocktailName;
-  //img
-  imgEl.src = cocktailData.strDrinkThumb;
+      //name
+      const cocktailName = cocktailData.strDrink;
+      nameEl.innerHTML = cocktailName;
+      //img
+      imgEl.src = cocktailData.strDrinkThumb;
 
-  //ingredients
+      //ingredients
 
-  for (let i = 1; i <= 15; i++) {
-    let index = "strIngredient" + i;
-    // console.log(cocktailData[index]);
-    if (!cocktailData[index]) {
-      break;
-    } else {
-      const listEl = document.createElement("li");
-      listEl.innerHTML = cocktailData[index];
-      ingredientsList.appendChild(listEl);
-    }
-  }
+      for (let i = 1; i <= 15; i++) {
+         let index = "strIngredient" + i;
+         // console.log(cocktailData[index]);
+         if (!cocktailData[index]) {
+            break;
+         } else {
+            const listEl = document.createElement("li");
+            listEl.innerHTML = cocktailData[index];
+            ingredientsList.appendChild(listEl);
+         }
+      }
 
-  //instruction
-  const stepsList = document.querySelector(".steps");
-  const split = cocktailData.strInstructions.split(".");
-  split.forEach((step) => {
-    if (step) {
-      const listEl = document.createElement("li");
-      listEl.innerHTML = step;
-      stepsList.appendChild(listEl);
-    }
-  });
-  return "hello";
-});
+      //instruction
+      const stepsList = document.querySelector(".steps");
+      const split = cocktailData.strInstructions.split(".");
+      split.forEach((step) => {
+         if (step) {
+            const listEl = document.createElement("li");
+            listEl.innerHTML = step;
+            stepsList.appendChild(listEl);
+         }
+      });
+   });
+}
 
 const button = document.querySelector("button");
-button.addEventListener("click", () => {
-  return promise.then(() => console.log("click"));
-});
+button.addEventListener("click", promiseFunc);
+
+window.onload = () => promiseFunc();
